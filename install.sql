4ab55fbfd5021232271187361d2742da6aaced61
raoulmunet <raoulmunet@gmail.com>
2026-09-23 18:51:51 +0300

    Add install.sql

---
+++ install.sql
@@ -0,0 +1,13 @@
+prompt Installing Oracle ETL Batch Framework...
+
+@sql/01_tables.sql
+@sql/02_indexes.sql
+@sql/03_demo_objects.sql
+@sql/04_sample_data.sql
+
+@plsql/pkg_etl_batch.pks
+@plsql/pkg_etl_batch.pkb
+@plsql/pkg_customer_load.pks
+@plsql/pkg_customer_load.pkb
+
+prompt Installation complete.
